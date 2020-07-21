50d0d69e9480349d23efcaae4833aaa1894bf126
coolbt88 <braedenbt88@gmail.com>
2020-07-20 22:12:07 -0400

    json file gone

--- Movie_Bot/Mov_Bot-Index.js
+++ Movie_Bot/Mov_Bot-Index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
+const config = require('d:/Discord/JSON_files/Mov_bot-config.json');
 const pre = config.prefix;
 
 client.on('ready', () => {
@@ -10,7 +10,7 @@ client.on('ready', () => {
 client.on('message', message =>{
     if (message.author.bot) return;
 
-    if(message.content.startsWith(`${pre} honk`)){
+    if(message.content.startsWith(`${pre}`)){
         message.reply("I'M WALKING HERE");
     }
 
