867daedd154fc99b9d6063acb593d7788c5c83bd
coolbt88 <braedenbt88@gmail.com>
2020-07-20 23:18:35 -0400

    Got JSON requets working
    Need to add channel lock function
    Role pings
    Role Adding
    Channel name changing
    Revert to og name after movie is done

--- Movie_Bot/Mov_Bot-Index.js
+++ Movie_Bot/Mov_Bot-Index.js
@@ -2,16 +2,37 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('d:/Discord/JSON_files/Mov_bot-config.json');
 const pre = config.prefix;
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 client.on('ready', () => {
     console.log('ready fool');
 });
 
+
 client.on('message', message =>{
     if (message.author.bot) return;
 
     if(message.content.startsWith(`${pre}`)){
-        message.reply("I'M WALKING HERE");
+
+        var tempTitle1 = (message.content).split("/");
+        var movieTitle = encodeURIComponent(tempTitle1[1]);
+        var movieUrl = 'http://www.omdbapi.com/?apikey=472eaa45&t=' +movieTitle;
+        console.log(movieUrl);
+
+        
+        let request = new XMLHttpRequest();
+        request.open('GET', movieUrl);
+        request.send();
+        request.onload = () => {
+            console.log(request);
+            if(request.status === 200){
+                let info = JSON.parse(request.responseText);
+                message.reply(info.Title);
+            }
+            else{
+                console.log(`error ${request.status} ${request.statusText}`);
+            }
+        }
     }
 
 });
