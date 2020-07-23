43aedca682c27fa8535750954cb9a2098f120b90
coolbt88 <braedenbt88@gmail.com>
2020-07-23 18:19:08 -0400

    added set up permissions using roles.
    Need to add role pinging using reactions and role creation

--- Movie_Bot/Mov_Bot-Index.js
+++ Movie_Bot/Mov_Bot-Index.js
@@ -11,41 +11,129 @@ client.on('ready', () => {
 
 
 var checkChannel;
+var admin;
 
 client.on('message', message =>{
     if (message.author.bot) return;
 
-    if(message.content.startsWith(`${pre}${mod}start`)){
+    if(message.content.startsWith(`${pre}${mod}setup`)){
+        setAdmin();
+        return;
+    }
+
+    if(message.content.startsWith(`${pre}${mod}set-channel`)){
         setupChannel();
         return;
     }
 
+    if(message.content.startsWith(`${pre}${mod}reminders`)){
+        sendReminder();
+        return;
+    }
+    
     function setupChannel(){
-        let channel = message.channel.id;
         let foundChannel = false;
 
-        client.channels.cache.get(channel).send('Alright. What channel would you like me to send my reminders to?');
-        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
-        collector.on('collect', message => {
-            client.channels.cache.forEach((channel) => {
-                if(message.content === channel.id){
-                    checkChannel = message.content.replace(/\D/g,'');
-                    foundChannel = true;
+        if(admin === undefined){
+            message.channel.send("Need to initialize bot first. type 'm/?setup' to set up Movie Bot")
+        }
+        else{
+            message.channel.send('Copy the ID of the channel you want me to send my announcements to: ');
+            const filter = m => m.author.id === message.author.id;
+            const collector = message.channel.createMessageCollector(filter, {max: 1, time: 30000});
+            collector.on('collect', m => {
+                client.channels.cache.forEach((channel) => {
+                    if(m.content === channel.id){
+                        checkChannel = m.content.replace(/\D/g,'');
+                        foundChannel = true;
+                    }
+                })
+                if(foundChannel){
+                    message.channel.send(('Ok. I\'ll send my reminders to <#' + checkChannel + '>.'));
                 }
-            })
-            if(foundChannel){
-                client.channels.cache.get(channel).send(('Ok. I\'ll send my reminders to <#' + checkChannel + '>.'));
-            }
-            else{
-                client.channels.cache.get(channel).send('Error.');
-            }
-            return;
+                else{
+                    message.channel.send('Error.');
+                }
+                return;
+        })}
+    }
+
+    function setAdmin(){
+        console.log(admin);
+        message.channel.send('Which role would you like to be able to edit my settings?');
+        const filter = m => m.author.id === message.author.id;
+        const collector = message.channel.createMessageCollector(filter, {max: 1, time: 30000});
+        collector.on('collect', m => {
+                let args = m.content.toLowerCase();
+                let { cache } = m.guild.roles;
+                let role = cache.find(role => role.name.toLowerCase() === args);
+                if(role){
+                    admin = args;
+                    message.channel.send(`${m} can now modify my settings`);
+                }
+                else{
+                    message.channel.send('Error.');
+                }
         })
+    }
+
+    function sendReminder(){
+        let m = "Hey guys! Interact with this messsage if you want to be pinged when we're watching a movie!"
+        
+        /*client.guild.roles.create({
+            data:{
+                name: 'Moviegoers',
+                color: 'BLUE',
+            },
+            reason: 'to ping people who want to watch movies'
+        })
+            .then(console.log);
+            */
+
+        if(checkChannel === undefined){
+            message.channel.send(m)
+                .then(messageReaction => {
+                    messageReaction.react('🎥');
+                })
+                .then(async function (message){
+                    await message.react('🎥')
+                })
+                const filter = (reaction, user) => {
+                    return ['🎥'].includes(reaction.emoji.name) && user.id === message.author.id;
+                };
+
+                const collector = message.createReactionCollector(filter, {time: 60000});
+
+                collector.on('collect', (reaction, reactionCollector) => {
+                    if (reaction.emoji.name === '🎥') {
+                        var role = message.guild.role.find(role => role.name === "Moviegoers")
+                        message.member.addRole(role);
+                    }
+                });
+        }
+        else{
+            checkChannel.send(m)
+                .then(messageReaction => {
+                    messageReaction.react('🎥');
+                });
+                message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+                .then(collected => {
+                    const reaction = collected.first();
+            
+                    if (reaction.emoji.name === '🎥') {
+                        var role = message.guild.role.find(role => role.name === "Moviegoers")
+                        message.member.addRole(role);
+                    }
+                })
+                .catch(collected => {
+                    message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
+                });
+    
+        }
     }
     
     if(message.content.startsWith(`${pre}`)){
         if(message.content.startsWith(`${pre}${mod}`)) return;
-        let channel = message.channel.id;
         var tempTitle1 = (message.content).split("/");
         var movieTitle = encodeURIComponent(tempTitle1[1]);
         var movieUrl = 'http://www.omdbapi.com/?apikey=472eaa45&t=' +movieTitle;
@@ -56,15 +144,19 @@ client.on('message', message =>{
         request.onload = () => {
             if(request.status === 200){
                 let info = JSON.parse(request.responseText);
-                var message = `We're going to be watching ${info.Title} soon!
+                var m = `We're going to be watching ${info.Title} soon!
                 Description: ${info.Plot}
                 Starring: ${info.Actors}
                 `
-                if(checkChannel === undefined){
-                    client.channels.cache.get(channel).send(message);
+                if(info.Title === undefined){
+                    message.channel.send("I couldn't find a movie that matched that description...")
+                }
+                
+                else if(checkChannel === undefined){
+                    message.channel.send(m);
                 }
                 else{
-                    client.channels.cache.get(checkChannel).send(message);
+                    checkChannel.send(m);
                 }
                
             }
---
+++ Movie_Bot/MovieBot.bat
@@ -0,0 +1,3 @@
+d:
+cd D:\Discord\Discord_Bots\Movie_Bot
+node Mov_Bot-Index.js
